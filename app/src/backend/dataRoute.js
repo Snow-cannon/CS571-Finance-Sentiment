16a26871adfabbea961aed0be0094e8dbcff1183
Snow-cannon <ofludlam@gmail.com>
2025-03-25 12:30:33 -0400

    Implements sending CSV files on seperate router

---
+++ app/src/backend/dataRoute.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+
+const router = express.Router();
+
+// Route for data files
+const dataRoot = path.join("src", "data");
+
+// For serving static data files
+router.use(express.static(dataRoot));
+
+// Test CSV
+router.get("/test", (req, res) => {
+  const options = {
+    root: dataRoot,
+  };
+
+  res.sendFile("test.csv", options);
+});
+
+export default router;
--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import dataRouter from "./dataRoute.js";
 
 // Create server
 const app = express();
 
 // Set up path information
 const staticRoot = path.join("src", "frontend");
-const dataRoot = path.join("src", "data");
 
 // Allow static file serving (serves all static files in the "frontend" directory)
 app.use(express.static(staticRoot));
-app.use(express.static(dataRoot));
+
+// Attach the data serving routes
+app.use("/data", dataRouter);
 
 // Enable CORS
 app.use(cors());
